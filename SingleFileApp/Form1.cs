cfade6b0c96df79204ac907bb507fa1843ddf9a0
erolkaldi <43037084+erolkaldi@users.noreply.github.com>
2023-01-12 13:54:09 +0300

    Add project files.

---
+++ SingleFileApp/Form1.cs
@@ -0,0 +1,17 @@
+using DevExpress.XtraEditors;
+
+namespace SingleFileApp
+{
+    public partial class Form1 : XtraForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
